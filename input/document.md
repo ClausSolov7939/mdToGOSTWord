86e5eaab505a943f8a020cd3bb31f9ad938b2216
ClausSolov7939 <claussolov7939@gmail.com>
2024-03-10 18:08:38 +0300

    Starting new project

---
+++ input/document.md
@@ -0,0 +1,10 @@
+# 1st Header
+
+1st Paragraph
+
+2nd Paragraph
+
+# 2nd Header
+## 3nd Header
+
+3rd Paragraph
--- src/main.py
+++ src/main.py
@@ -1,20 +1,37 @@
-from docx import Document
-from docx.enum.style import WD_STYLE_TYPE
-from docx.shared import Pt, Mm
 import json
 
-fin = open("../input/document.md", "r")
-styleFile = open("style.json", encoding='utf-8')
+from docx import Document
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.text import WD_LINE_SPACING
+from docx.shared import Pt, Mm
+
+
+setupFile = open("ОтДуксина.json", encoding='utf-8')
+setupData = json.load(setupFile)
 
 document = Document()
-styles = document.styles
 
-styleData = json.load(styleFile)
+section = document.sections[0]
+
+pageData = setupData["Page Setup"]
+
+# section.page_height = Mm(297)
+# section.page_width = Mm(210)
+section.left_margin = Mm(pageData["left_margin"])
+section.right_margin = Mm(pageData["right_margin"])
+section.top_margin = Mm(pageData["top_margin"])
+section.bottom_margin = Mm(pageData["bottom_margin"])
+
+styles = document.styles
+styleData = setupData["Styles"]
+
+for i in styles:
+    styles[i.name].delete()
 
 for newStyle in styleData.keys():
     currentStyle = styles.add_style(newStyle, WD_STYLE_TYPE.PARAGRAPH)
-    print(currentStyle)
-    # -------- paragraph setting -----------
+    # print(currentStyle)
+    # -------- font setting -----------
     font_format = currentStyle.font
     font_style = styleData[newStyle]["font"]
     font_format.name = font_style["name"]
@@ -26,70 +43,58 @@ for newStyle in styleData.keys():
     # -------- paragraph setting -----------
     paragraph_format = currentStyle.paragraph_format
     paragraph_style = styleData[newStyle]["paragraph"]
-    # "alignment": "Left",
+    # 0 - left, 1 - center, 2 - right, 3 - justify
 
-    paragraph_format.first_line_indent = Mm(paragraph_style["first_line_indent"])
+    alignment = {"left": 0, "center": 1, "right": 2, "justify": 3}
+
+    paragraph_format.alignment = alignment[paragraph_style["alignment"]]
+    print(paragraph_style["alignment"], paragraph_format.alignment)
+    paragraph_format.first_line_indent = Mm(
+        paragraph_style["first_line_indent"])
     paragraph_format.left_indent = Mm(paragraph_style["left_indent"])
     paragraph_format.right_indent = Mm(paragraph_style["right_indent"])
     paragraph_format.space_before = Mm(paragraph_style["space_before"])
     paragraph_format.space_after = Mm(paragraph_style["space_after"])
-    paragraph_format.line_spacing = Mm(paragraph_style["line_spacing"])
+    if paragraph_style["line_spacing"] == 1.5:
+        paragraph_format.line_spacing_rule = WD_LINE_SPACING.ONE_POINT_FIVE
+    elif paragraph_style["line_spacing"] == 1:
+        paragraph_format.line_spacing_rule = WD_LINE_SPACING.SINGLE
     # Запрет висящих строк
-    currentStyle.widow_control = paragraph_style["widow_control"]
+    paragraph_format.widow_control = paragraph_style["widow_control"]
     # Не отрывать от следующего
-    currentStyle.keep_with_next = paragraph_style["keep_with_next"]
+    paragraph_format.keep_with_next = paragraph_style["keep_with_next"]
     # Не разрывать абзац
-    currentStyle.keep_together = paragraph_style["keep_together"]
+    paragraph_format.keep_together = paragraph_style["keep_together"]
     # с новой страницы
-    currentStyle.page_break_before = paragraph_style["page_break_before"]
-    print(paragraph_style["page_break_before"], currentStyle.page_break_before)
+    paragraph_format.page_break_before = paragraph_style["page_break_before"]
 
+# print(document.styles["Заголовок 1"])
+# print(document.styles["Заголовок 1"].paragraph_format.page_break_before)
 
-print(document.styles["Заголовок 1"])
-print(document.styles["Заголовок 1"].paragraph_format.page_break_before)
+for i in styles:
+    print(i.name)
 
-
+fin = open("../input/untitled.md", "r", encoding="utf-8")
 for line in fin:
-    splitedLine = line.split()
-    print(splitedLine)
-    if len(splitedLine) != 0:  # if line is not empty
-        if "#" in splitedLine[0]:  # if line contains a Header
-            header = document.add_paragraph(splitedLine[1:])
-            header.style = document.styles[f'Заголовок {len(splitedLine[0])}']
-# p = document.add_paragraph('A plain paragraph having some ')
-# p.add_run('bold').bold = True
-# p.add_run(' and some ')
-# p.add_run('italic.').italic = True
-#
-# document.add_heading('Heading, level 1', level=1)
-# document.add_paragraph('Intense quote', style='Intense Quote')
-#
-# document.add_paragraph(
-#     'first item in unordered list', style='List Bullet'
-# )
-# document.add_paragraph(
-#     'first item in ordered list', style='List Number'
-# )
-#
-# # document.add_picture('monty-truth.png', width=Inches(1.25))
-#
-# records = (
-#     (3, '101', 'Spam'),
-#     (7, '422', 'Eggs'),
-#     (4, '631', 'Spam, spam, eggs, and spam')
-# )
-#
-# table = document.add_table(rows=1, cols=3)
-# hdr_cells = table.rows[0].cells
-# hdr_cells[0].text = 'Qty'
-# hdr_cells[1].text = 'Id'
-# hdr_cells[2].text = 'Desc'
-# for qty, id, desc in records:
-#     row_cells = table.add_row().cells
-#     row_cells[0].text = str(qty)
-#     row_cells[1].text = id
-#     row_cells[2].text = desc
-#
-# document.add_page_break()
+#    print("[ ",line," ]")
+    if line!="\n":
+        # Заголовки
+        if "#" in line.split(" ")[0]:
+            head=line.replace("#","").replace("\n","")
+            level = len(line.split(" ")[0])
+            document.add_paragraph(head[1:], style=f"Head {level}")
+        elif line[0]=="!":
+            imagePath = "../input/"+line.replace("![[","").replace("]]\n","")
+            print(imagePath)
+#            doc.add_paragraph("Picture",style="Normal")
+            p = document.add_picture(imagePath, width=Mm(153))
+#            p.style.paragraph_format.alignment = 1
+            last_paragraph = document.paragraphs[-1]
+            last_paragraph.style = "Picture"
+            document.add_paragraph(fin.readline(), style="PictureName")
+        else:
+            document.add_paragraph(line.replace("\n",""),style="Main Text")
+    else:
+        print("empty")
 
 document.save('../output/demo.docx')
